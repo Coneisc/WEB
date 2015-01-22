facf426bab305a0cbef6887f48f6a3a128be2d8d
Cristhianrg <cristhianr.gomez@gmail.com>
2015-01-22 12:30:38 -0500

    test

---
+++ index12.php
@@ -0,0 +1,208 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>CONEISC - INICIO</title>
+		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/jquery.dropotron.min.js"></script>
+		<script src="js/jquery.scrolly.min.js"></script>
+		<script src="js/jquery.onvisible.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+		<link rel="stylesheet" href="css/skel.css" />
+		<link rel="stylesheet" href="css/style.css" />
+		<link rel="stylesheet" href="css/style-desktop.css" />
+		<link rel="stylesheet" href="css/style-noscript.css" />
+		<link rel="stylesheet" href="css/nstyles.css" />
+		<link rel="stylesheet" href="css/style0302.css" />
+		<link rel="stylesheet" href="css/estilos.css" />
+		<script>
+			function toogle(a,b,c)
+			{
+				if(a=="none")
+				{
+					$("#iniciov")[0].pause();
+				}
+			  document.getElementById(b).style.display=a;
+			  document.getElementById(c).style.display=a;
+			}
+		</script>
+	</head>
+	<body class="homepage" onload="toogle('block','modaljs','ventana')">
+		<!-- Modal de video -->
+		<div id="modaljs" style="display:none">
+			<div id="ventana" class="contenedorjs" style="display:none">
+				<h2 style="padding-bottom: 0px; text-align: center;">CONEISC XXIII 2015</h2> 
+				<video id="iniciov" class="video" autoplay  controls="controls" style="margin-left: 0px; margin-right: 0px;height: 420px;"><source src="img/coneisc.mp4" type="video/mp4"></video>
+				<a href="#close" title="Cerrar" onclick="toogle('none','modaljs','ventana')" >Close</a>
+			</div>
+		</div>
+		<!--- fin de modal -->
+
+		<!-- Header -->
+			<div id="header">
+						
+				<!-- Inner -->
+					<div class="inner">
+						<header>
+							<img width="400px" height="200px" src="img/logo2.png">
+							<hr />
+							<p class="txt-top">"Emprendimiento Tecnologico en el Siglo XXI, CONEISC XXIII 2015"</p>
+						</header>
+						<footer>
+							<a href="#banner" class="buttonhead circled scrolly">Bienvenido</a>
+						</footer>
+					</div>
+				<!-- Nav -->
+				<?php include 'include/menutop.php';?>
+
+			</div>
+		<!-- Banner -->
+			<section id="banner">
+				<header>
+					<p class="frases">"El diseño es el alma de todo lo creado por el hombre." <br> Steve Jobs</p>
+				</header>
+			</section>
+			<section id="programacion" class="carousel">
+				<div class="reel">
+					<article>
+						<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" 
+						style="width:360px" /></a>
+						<header>
+							<h3><a href="#">PhD. EDWARD KNIGHTLY </a></h3>
+						</header>
+						<p>Diverse spectrum and the next wireless Internet</p>							
+					</article>
+					<article>
+						<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" 
+						style="width:360px" /></a>
+						<header>
+							<h3><a href="#"></a>ING. JOSE PABLO ESQUIVEL </h3>
+						</header>
+						<p>El Internet de las cosas - CISCO <br>.</p>							
+					</article>
+					<article>
+						<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" 
+						style="width:360px" /></a>
+						<header>
+							<h3><a href="#"></a>ING. JORGE PEREZ </h3>
+						</header>
+						<p>Bases de Datos de Web Semántica<br>.</p>							
+					</article>
+					<article>
+						<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" 
+						style="width:360px" /></a>
+						<header>
+							<h3><a href="#"></a>ING. JUAN CARLOS ESPINOZA </h3>
+						</header>
+						<p>Web-Escale Data Center - Integración para empresas peruanase</p>							
+					</article>
+					<article>
+						<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" 
+						style="width:360px" /></a>
+						<header>
+							<h3><a href="#"></a>ING. ROMULO BENITEZ </h3>
+						</header>
+						<p>Plataformas online para interconectar hardware</p>							
+					</article>
+					<article>
+						<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" 
+						style="width:360px" /></a>
+						<header>
+							<h3><a href="#"></a>ING. MIGUEL VELARDE </h3>
+						</header>
+						<p>Mejores prácticas en el diseño e implementación de un Centro de Datos</p>							
+					</article>
+					<article>
+						<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" 
+						style="width:360px" /></a>
+						<header>
+							<h3><a href="#"></a>ING. GUILLERMO CALDERON </h3>
+						</header>
+						<p>Minería de Procesos, un paso para la ciencia de datos</p>							
+					</article>
+					<article>
+						<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" 
+						style="width:360px" /></a>
+						<header>
+							<h3><a href="#"></a>ING. KARINA ROSAS PAREDES </h3>
+						</header>
+						<p>Detección de intrusos en la capa de enlace de datos para WLAN basado en open hardware</p>							
+					</article>
+				</div>
+			</section>	
+			<section id="banner">
+						<center class="titazul">Partners</center><br>
+						<!-- Informacion sobre los ponentes    -->
+						<center>
+							<div class="row" style="margin-left: 3%; margin-top: 0px; margin-right: 3%;">
+								<ul class="cont-paners">
+									 <li class="list-paners">
+										 <img src="img/ge-image.jpg" class="imghover">
+										 <div class="info">
+											 <a href="http://www.ge.com/"><img src="img/logo-ge.png" alt=""></a>
+										 </div>
+									 </li>
+									 <li class="list-paners">
+										 <img src="img/inte-image.jpg" class="imghover">
+										 <div class="info">
+											 <a href="http://www.integrity.pe/"><img src="img/logo-inte.png" alt=""></a>
+										 </div>
+									 </li>
+									 <li class="list-paners">
+										 <img src="img/sp-image.jpg" class="imghover">
+										 <div class="info">
+											 <a href="http://www.silver-peak.com/"><img src="img/logo-sp.png" alt=""></a>
+										 </div>
+									 </li>
+									 <li class="list-paners">
+										 <img src="img/ge-image.jpg" class="imghover">
+										 <div class="info">
+											 <a href="http://www.ge.com/"><img src="img/logo-ge.png" alt=""></a>
+										 </div>
+									 </li>
+									 <li class="list-paners">
+										 <img src="img/ge-image.jpg" class="imghover">
+										 <div class="info">
+											 <a href="http://www.ge.com/"><img src="img/logo-ge.png" alt=""></a>
+										 </div>
+									 </li>
+									 <li class="list-paners">
+										 <img src="img/inte-image.jpg" class="imghover">
+										 <div class="info">
+											 <a href="http://www.integrity.pe/"><img src="img/logo-inte.png" alt=""></a>
+										 </div>
+									 </li>
+									 <li class="list-paners">
+										 <img src="img/sp-image.jpg" class="imghover">
+										 <div class="info">
+											 <a href="http://www.silver-peak.com/"><img src="img/logo-sp.png" alt=""></a>
+										 </div>
+									 </li>
+									 <li class="list-paners">
+										 <img src="img/ge-image.jpg" class="imghover">
+										 <div class="info">
+											 <a href="http://www.ge.com/"><img src="img/logo-ge.png" alt=""></a>
+										 </div>
+									 </li>
+								 </ul>
+							</div>
+						</center>
+			</section>
+			<section id="Noticias" class="clear" style="margin-bottom: 0px;">
+				<div id="twitter">
+					<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/CONEISCXXIII" data-widget-id="549336861702119426">Tweets por el @CONEISCXXIII.</a>
+					<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+				</div>	
+			</section>
+			
+		<!-- Footer -->
+			<?php include 'include/footer.php';?>
+
+	</body>
+</html>
