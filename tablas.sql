c7e9078472fc88bbc419cb119a418a0c7d3cc39d
Cristhianrg <cristhianr.gomez@gmail.com>
2015-02-13 16:04:12 -0500

    Update tablas.sql

--- tablas.sql
+++ tablas.sql
@@ -44,3 +44,23 @@ CREATE TABLE IF NOT EXISTS `apeisc_coneisc`.`usuario_horario` (
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
+
+
+
+-- -----------------------------------------------------
+-- Procedure : insertarinscripcion
+-- -----------------------------------------------------
+
+DELIMITER //
+
+CREATE PROCEDURE insertarinscripcion(in ins_dni CHAR(8) ,in ins_nom VARCHAR(100) ,in ins_ape VARCHAR(100) ,in ins_mai VARCHAR(200) ,in ins_pho CHAR(11) ,in ins_eda INT(2) ,in ins_sex CHAR(1) ,in ins_ocu VARCHAR(3) ,in cod_ciu VARCHAR(3) ,in cod_uni VARCHAR(3) ,in ins_tra VARCHAR(15) ,in cod_est VARCHAR(3),in cla_dni VARCHAR(200),in use_pass VARCHAR(200))
+BEGIN
+INSERT INTO `inscripcion`(`ins_dni`, `ins_nom`, `ins_ape`, `ins_mai`, `ins_pho`, `ins_eda`, `ins_sex`, `ins_ocu`, `cod_ciu`, `cod_uni`, `ins_tra`, `cod_est`) VALUES (ins_dni, ins_nom, ins_ape, ins_mai, ins_pho, ins_eda, ins_sex, ins_ocu, cod_ciu, cod_uni, ins_tra, cod_est);
+
+INSERT INTO `clave`(`cla_dni`, `cla_pass`) VALUES (cla_dni,use_pass);
+
+END //
+
+DELIMITER ;
+
+
